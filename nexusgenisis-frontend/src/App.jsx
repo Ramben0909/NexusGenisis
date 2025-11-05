5e65e914bc9b5e0167b585b62c1312ceccb21d04
PROJITA KAR <projita2003kar@gmail.com>
2025-11-06 03:22:53 +0530

    integrated login/register with backend and dynamic user profile

--- nexusgenisis-frontend/src/App.jsx
+++ nexusgenisis-frontend/src/App.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './layout/MainLayout';
-import Homepage from './pages/Homepage';
-import StatsPage from './pages/StatsPage';
-import TrendsScopePage from './pages/TrendsScopePage';
-import ProfilePage from './pages/ProfilePage';
-import NotFoundPage from './pages/NotFoundPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Homepage from "./pages/Homepage";
+import StatsPage from "./pages/StatsPage";
+import TrendsScopePage from "./pages/TrendsScopePage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AuthPages from "./pages/AuthPages";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <Router>
-      <MainLayout>
-        <Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <Routes>
+        {/* Public route */}
+        <Route path="/auth" element={<AuthPages />} />
+
+        {/* Protected layout */}
+        <Route
+          element={
+            <ProtectedRoute>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route path="/" element={<Homepage />} />
           <Route path="/stats" element={<StatsPage />} />
           <Route path="/trends" element={<TrendsScopePage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </MainLayout>
+        </Route>
+      </Routes>
     </Router>
   );
 }
